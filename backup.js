e71ce96723b776e1105e22195317bbef6e333b05
lan <i2534@163.com>
2024-02-12 20:15:12 +0800

    Update backup.js
    console.log add timestamp output

--- backup.js
+++ backup.js
@@ -2,6 +2,20 @@
 const fs = require('fs');
 const path = require('path');
 
+//format date to yyyy-MM-dd HH:mm:ss
+Date.prototype.format = function () {
+    const date = this;
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    const hours = ("0" + date.getHours()).slice(-2);
+    const minutes = ("0" + date.getMinutes()).slice(-2);
+    const seconds = ("0" + date.getSeconds()).slice(-2);
+    return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+}
+
+console.log = console.log.bind(console, new Date().format() + ' -');
+
 const api = Object.freeze({
     //https://alist.nn.ci/zh/guide/api/fs.html
     fs: Object.freeze({
